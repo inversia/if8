8a46a87375a8232496cfd9619b5ed09b27ae132b
inversia <mrs.shpritz@gmail.com>
2020-08-25 03:25:05 +0300

    header & icons add

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import Header from '~/Header'
 
 export default function App () {
     return <>
-        <h1 style={{color:'red', fontSize:'12vw'}}>Hello IF8</h1>
-        <div style={{color:'red', fontSize:'2vw'}}>DFSGSDFHSFGHSFGHFGDH</div>
+        <Header/>
     </>
 }
---
+++ src/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './index.css'
+
+export default function Header () {
+    return <div className='header-wrapper'>
+        <div className='tagline'></div>
+        <div className='logo'></div>
+    </div>
+}
