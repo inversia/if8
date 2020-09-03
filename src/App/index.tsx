82714296f804a00a2b3bb48e0c83bc66cfe06ba1
inversia <mrs.shpritz@gmail.com>
2020-09-04 01:01:38 +0300

    Footer add

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '~/Header'
 import Menu from '~/Menu'
+import Main from '~/Main'
+import Footer from '~/Footer'
 // import { useRoutes, usePath } from 'hookrouter'
 
 export default function App () {
     return <>
         <Header/>
         <Menu />
+        <Main />
+        <Footer />
     </>
 }
+
+// import ItemInfo from "../ItemInfo"
