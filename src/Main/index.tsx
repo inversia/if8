e1df1ec5fe1c917c60fe83d5697425d3a2b26c1c
inversia <mrs.shpritz@gmail.com>
2020-09-04 02:33:35 +0300

    Main add

---
+++ src/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+import Novelties from '~/Novelties'
+
+export default function Main() {
+  return (
+    <>
+      <h1>НОВИНКИ</h1>
+      <Novelties />
+    </>
+  );
+}
