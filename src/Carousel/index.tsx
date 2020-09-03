c81fe77a8964b1b5dfbfe2ab3e1f6ad17d9edd1f
inversia <mrs.shpritz@gmail.com>
2020-09-04 02:33:57 +0300

    Carousel added

---
+++ src/Carousel/index.tsx
@@ -0,0 +1,57 @@
+import React, { useRef, useState, useCallback } from 'react'
+import './index.css'
+import classname from 'classnames'
+
+export default function Carousel ({ children = [] as React.ReactNode[] }) {
+  const [currentItem, setCurrentItem] = useState (0)
+  const scrollerContainer = useRef<HTMLDivElement> ()
+
+  const onScroll = useCallback (
+    (event: React.UIEvent) => {
+      const el = event.target as Element
+      const elementWidth = el.scrollWidth
+      const numChildren = el.childElementCount
+      const itemWidth = elementWidth / numChildren
+      const itemPosition = Math.round (el.scrollLeft / itemWidth)
+
+      setCurrentItem (itemPosition)
+    },
+    [setCurrentItem]
+  )
+
+  const dotClicked = useCallback ((i: number) => {
+    const { current: el } = scrollerContainer
+
+    el.scrollTo ({
+      left: i * (el.scrollWidth / el.childElementCount),
+      behavior: 'smooth'
+    })
+  }, [])
+
+  return (
+    <div className="carousel">
+      <div
+        className="carousel-scroller"
+        onScroll={onScroll}
+        ref={scrollerContainer}
+      >
+        {children.map ((x, i) => (
+          <div className="carousel-item" key={i}>
+            {x}
+          </div>
+        ))}
+      </div>
+      <div className="carousel-pagination">
+        {children.map ((_, i) => (
+          <div
+            className={classname ('carousel-page-btn', {
+              active: i === currentItem
+            })}
+            key={i}
+            onClick={() => dotClicked (i)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
