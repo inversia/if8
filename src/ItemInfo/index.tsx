3aa24b99e45ed3b1b116851cf225d11c71466fb4
inversia <mrs.shpritz@gmail.com>
2020-09-21 00:04:16 +0300

    ItemInfo fix

--- src/ItemInfo/index.tsx
+++ src/ItemInfo/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './index.css'
 import { products, FilterProps } from '~/data'
-// import images from '~/images'
+import images from '~/images'
 
 export function ItemInfo ({ id }: FilterProps) {
 
@@ -9,21 +9,34 @@ export function ItemInfo ({ id }: FilterProps) {
 
 	return <>
 		<div className='item-wrapper'>
-			<div className='illustration'></div>
+			<div className='illustration' style={{ backgroundImage: `url(${images[choosenProduct.img]})` }}></div>
 			<div className='item-info'>
 				<ul>
-					<label>Описание</label>
-					<li>{choosenProduct.description}</li>
-					<label>Артикул</label>
-					<li>{choosenProduct.partNumber}</li>
-					<label>Проба</label>
-					<li>{choosenProduct.probe}</li>
-					<label>Наличие</label>
-					<li></li>
-					<label>Цена</label>
-					<li>{choosenProduct.price}</li>
-					<label>Цвета</label>
+					<li>
+						<label>Описание</label>
+						<span>{choosenProduct.description}</span>
+					</li>
+					<li>
+						<label>Артикул</label>
+						<span>{choosenProduct.partNumber}</span>
+					</li>
+					<li>
+						<label>Проба</label>
+						<span>{choosenProduct.probe}</span>
+					</li>
+					<li>
+						<label>Наличие</label>
+						<span>{choosenProduct.availability}</span>
+					</li>
+					<li>
+						<label>Цена</label>
+						<span>{choosenProduct.price}</span>
+					</li>
+					<li>
+						<label>Цвета</label>
+					</li>
 				</ul>
+				<button>КУПИТЬ</button>
 			</div>
 		</div>
 	</>
