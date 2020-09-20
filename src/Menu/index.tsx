495f59425a4a41565d3a638d3b2da0f4a80fd24c
Vitalik Gordon <v-gordon@yandex-team.ru>
2020-09-21 00:05:49 +0300

    fix

--- src/Menu/index.tsx
+++ src/Menu/index.tsx
@@ -1,8 +1,9 @@
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useRef, useLayoutEffect, useState, useCallback } from 'react'
 import './index.css'
 import cls from 'classnames'
 import { A, usePath } from 'hookrouter'
 import { Category, subcategories, materials, FilterProps } from '~/data'
+import {useOnClickOutside} from 'use-hooks'
 // import RubberSlider from '@shwilliam/react-rubber-slider'
 import '@shwilliam/react-rubber-slider/dist/styles.css'
 
@@ -36,38 +37,43 @@ function MenuLink ({
     )
 }
 
-function parsePath (path: string): { category?: Category } {
-    const [, page, category] = path.split ('/')
-    return {
-        category: page === 'items' ? (category as Category) : undefined
-    }
-}
-
 export function Menu ({ category, subcategory, material, id }: FilterProps) {
 
     const [value, setValue] = useState (0.5)
 
-    const [dropdownVisible, setDropdownVisible] = useState (false)
+    const [dropdownCategory, setDropdownCategory] = useState<Category|null> ()
     
-    const toggleDropdown = (path: string) => {
-
-        if (dropdownVisible && category === parsePath (path).category) {
-            setDropdownVisible (false)
+    const toggleDropdown = (newCategory: Category|null) => {
+        if (dropdownCategory && dropdownCategory === newCategory) {
+            setDropdownCategory (null)
         } else {
-            setDropdownVisible (true)
+            setDropdownCategory (newCategory)
         }
     }
 
+    const menuContainerRef = useRef<HTMLDivElement> ()
+    useOnClickOutside (menuContainerRef, () => {
+        setDropdownCategory (null)
+    })
+
+    const hideIfClickedAtBottom = useCallback ((e: React.MouseEvent) => {
+        const distanceFromBottom = e.currentTarget.getBoundingClientRect ().bottom - e.clientY
+        const distanceFromBottomRel = distanceFromBottom / e.currentTarget.clientHeight // relative to height (0...1)
+        if (distanceFromBottomRel < 0.24) {
+            setDropdownCategory (null)
+        }
+    }, [])
+
     useLayoutEffect (() => {
-        if (!category) setDropdownVisible (false)
+        if (!category) setDropdownCategory (null)
     }, [category])
 
-    return <>
+    return <div ref={menuContainerRef} className="menu-container">
         <div className='menu'>
             <ul>
                 <MenuLink path='/about'>о компании</MenuLink>
-                <MenuLink path='/items/jewellery' onClick={toggleDropdown}>ювелирные украшения</MenuLink>
-                <MenuLink path='/items/interior'  onClick={toggleDropdown}>интерьер</MenuLink>
+                <li onClick={() => toggleDropdown ('jewellery')}>ювелирные украшения</li>
+                <li onClick={() => toggleDropdown ('interior')}>интерьер</li>
                 <MenuLink path='/'  onClick={() => {
                                         setTimeout (() => smoothScrollTo ('events'), 100)
                                     }}>события</MenuLink>
@@ -75,29 +81,29 @@ export function Menu ({ category, subcategory, material, id }: FilterProps) {
                 <MenuLink path='/cart'>сделать заказ</MenuLink>
             </ul>
         </div>
-        <div className={ cls ('dropdown-menu-container', { visible: dropdownVisible })}>
+        <div className={ cls ('dropdown-menu-container', { visible: !!dropdownCategory })} onClick={hideIfClickedAtBottom}>
             <div className='dropdown-menu'>
                 <div className='categories'>
                     <label>Категории</label>
                     <ul>
-                        <MenuLink path={`/items/${category}/exclusive`}>эксклюзивные украшения</MenuLink>
+                        <MenuLink path={`/items/${dropdownCategory}/exclusive`}>эксклюзивные украшения</MenuLink>
                         <div className='subcategories'>{
-                            category && Object.entries (subcategories[category]).map (([subcategory, label]: [string, string]) =>
-                                <MenuLink path={`/items/${category}/${subcategory}`} key={ subcategory }>{ label }</MenuLink>
+                            dropdownCategory && Object.entries (subcategories[dropdownCategory]).map (([subcategory, label]: [string, string]) =>
+                                <MenuLink path={`/items/${dropdownCategory}/${subcategory}`} key={ subcategory }>{ label }</MenuLink>
                             )
                         }</div>
-                        <MenuLink path={`/items/${category}/corporate`}>корпоративные подарки</MenuLink>
-                        <MenuLink path={`/items/${category}/all`}>все</MenuLink>
+                        <MenuLink path={`/items/${dropdownCategory}/corporate`}>корпоративные подарки</MenuLink>
+                        <MenuLink path={`/items/${dropdownCategory}/all`}>все</MenuLink>
                     </ul>
                 </div>
                 <div className='materials'>
                     <label>Материал</label>
                     <ul>{
-                        category && Object.entries (materials[category]).map (([material, label]: [string, string]) =>
-                            <MenuLink path={`/items/${category || 'all'}/${subcategory || 'all'}/${material}`} key={ material }>{ label }</MenuLink>
+                        dropdownCategory && Object.entries (materials[dropdownCategory]).map (([material, label]: [string, string]) =>
+                            <MenuLink path={`/items/${dropdownCategory}/${subcategory || 'all'}/${material}`} key={ material }>{ label }</MenuLink>
                         )
                     }
-                        <MenuLink path={`/items/${category || 'all'}/${subcategory || 'all'}`} key={ material }>все</MenuLink>
+                        <MenuLink path={`/items/${dropdownCategory}/${subcategory || 'all'}`} key={ material }>все</MenuLink>
                     </ul>
                 </div>
                 <div className='price'>
@@ -107,5 +113,5 @@ export function Menu ({ category, subcategory, material, id }: FilterProps) {
                 </div>
             </div>
         </div>
-    </>
+    </div>
 }
