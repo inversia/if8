f2d67f2f17bbb87312ad89c271dc40cbd6e06afb
inversia <mrs.shpritz@gmail.com>
2020-09-21 15:15:52 +0300

    background changes

--- src/About/index.tsx
+++ src/About/index.tsx
@@ -1,14 +1,16 @@
-import React, { createContext, useContext, useState} from 'react'
+import React from 'react'
 import './index.css'
 
 export default function About () {
     return <div className='about-container'>
-        <h1>О НАС</h1>
-        <p>Наступил момент, когда стоит <span style={{fontSize:'3vh'}}>подумать о сохранении</span> прикладного искусства, как <span style={{fontSize:'5vh'}}>кода нашей цивилизации</span>.</p>
-        <p> Компания IF8 ставит своей задачей, <span style={{fontSize:'5vh'}}>объединить талантливых художников</span> России, работающих в разных областях прикладного искусства.
-        <span>Дать</span> <span style={{fontSize:'6vh'}}>возможность</span> мастерам <span style={{fontSize:'8vh'}}>выразить</span> <span style={{fontSize:'3vh'}}>себя</span> в современных творческих работах, и тем самым обогатить наше культурное наследие.</p>
-        <p>IF8 -это вопрос о бесконечности.<span style={{fontSize:'5.3vw'}}>Бесконечности <span style={{fontSize:'8.5vh'}}>творческих</span> мыслей</span> наших художников.</p>
-        <p>IF8 - Бесконечность <span style={{fontSize:'5vh'}}>направлений</span>, в которых работают творческие люди.</p>
-        <p>IF8 - Бесконечность <span style={{fontSize:'3.5vh'}}>технологий</span>, которые мы черпаем из прошлого, сочетая их с <span style={{fontSize:'6vh'}}>современными</span>.</p>
+        <div className='about'>
+            <h1>О НАС</h1>
+            <p>Наступил момент, когда стоит <span style={{fontSize:'3vh'}}>подумать о сохранении</span> прикладного искусства, как <span style={{fontSize:'5vh'}}>кода нашей цивилизации</span>.</p>
+            <p> Компания IF8 ставит своей задачей, <span style={{fontSize:'5vh'}}>объединить талантливых художников</span> России, работающих в разных областях прикладного искусства.
+            <span>Дать</span> <span style={{fontSize:'6vh'}}>возможность</span> мастерам <span style={{fontSize:'8vh'}}>выразить</span> <span style={{fontSize:'3vh'}}>себя</span> в современных творческих работах, и тем самым обогатить наше культурное наследие.</p>
+            <p>IF8 -это вопрос о бесконечности.<span style={{fontSize:'5.3vw'}}>Бесконечности <span style={{fontSize:'8.5vh'}}>творческих</span> мыслей</span> наших художников.</p>
+            <p>IF8 - Бесконечность <span style={{fontSize:'5vh'}}>направлений</span>, в которых работают творческие люди.</p>
+            <p>IF8 - Бесконечность <span style={{fontSize:'3.5vh'}}>технологий</span>, которые мы черпаем из прошлого, сочетая их с <span style={{fontSize:'6vh'}}>современными</span>.</p>
+        </div>
     </div>
 }
