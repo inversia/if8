5ea41725914971b55ecaab0cf03825d8f5adb089
inversia <mrs.shpritz@gmail.com>
2020-09-29 05:10:01 +0300

    NAvigation bar add

---
+++ src/NavigationBar/index.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useRef, useLayoutEffect, useState, useCallback, useContext } from 'react'
+import './index.css'
+// import cls from 'classnames'
+import { A, usePath } from 'hookrouter'
+
+
+export default function NavigationBar () {
+
+    const [,, category, subcategory, material ] = usePath ().split ('/')
+
+    const navigationDict: Record<string, string> = {
+
+        jewellery: 'ювелирные украшения',
+        interior: 'интерьер',
+        rings: 'кольца',
+        bracelets: 'браслеты',
+        earrings: 'серьги',
+        neck: 'цепочки и подвески',
+        brooches: 'броши',
+        corporate: 'корпоративные',
+        exclusive: 'эксклюзив',
+        vases: 'вазы',
+        tables:'столы',
+        chairs:'стулья',
+        statuettes: 'статуэтки',
+        gold: 'золото',
+        silver: 'серебро',
+        platinum: 'платина',
+        wood: 'дерево',
+        metal:'металл',
+        ceramic:'керамика',
+        glass:'стекло',
+        steel:'сталь',
+        all: 'все',
+        undefined: ''
+    }
+
+    const purePath = [navigationDict[category] , navigationDict[subcategory], navigationDict[material]].filter ( x => x !== undefined && ' ')
+    const joinedPath = purePath.join (' / ')
+    const finalPath = joinedPath.slice (0, joinedPath.length - 1 )
+
+    return <>
+        <div className='navigation-bar'>{ joinedPath }</div>
+    </>
+}
