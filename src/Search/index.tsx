c313ee9bc823417a439be93ee196c5993faec890
inversia <mrs.shpritz@gmail.com>
2020-10-15 02:35:48 +0300

    novelties size change

--- src/Search/index.tsx
+++ src/Search/index.tsx
@@ -4,6 +4,8 @@ import debounce from 'lodash.debounce'
 import './index.css'
 import { products, Product } from '~/data'
 import images from '~/images'
+import { Link } from 'react-router-dom'
+import { useOnClickOutside } from 'use-hooks'
 
 export default function Search ({
     
@@ -14,6 +16,7 @@ export default function Search ({
     error = ''}) {
 
     const [input, setInput] = useState<string> (value)
+    const [hideSearchItems, setHidesearchItems] = useState<boolean|undefined> ()
 
     if (!buttonText) {
         onChange = debounce (onChange, 1000)
@@ -27,32 +30,39 @@ export default function Search ({
 
     const filteredProducts = products.filter (x => itemFilter (x))
 
+
+
     return (
 
         <div className={ cls ('search', className, { 'error': error && input === value }) }>
             <input
                 type='text'
+                autoComplete='off'
+                autoCorrect='off'
                 name='repo'
                 spellCheck={ false }
                 value={ input }
                 onChange={ ({ target: { value }}) => {
                     setInput (value)
+                    setHidesearchItems (true)
                     if (!buttonText) { onChange (value) }
                 }}
             />
 
-                {console.log (filteredProducts)}
-                {input && <ul className='search-list'>
-                    {filteredProducts.map (prdct => <li className='filtered-item' key={prdct.id}>
+                {hideSearchItems !== false && (input && (filteredProducts.length > 0)) && <ul className='search-list'>
+                    {filteredProducts.map (prdct => <Link   className='filtered-item' 
+                                                            key={prdct.id} to={`/item/${prdct.id}`} 
+                                                            onClick={() => { setHidesearchItems (false); setInput ('')}}>
                                                         <div style={{ backgroundImage: `url(${images[prdct.img]})`}}
                                                              className='filtered-item-background'/>
                                                         <div className='filtered-item-info-wrapper'>
-                                                            <div className='filtered-item-description'><p>
-                                                                <strong>{prdct.title}</strong>{prdct.description && (', ' + prdct.description)}</p>
+                                                            <div className='filtered-item-description'>
+                                                                <p><strong>{prdct.title}</strong></p>
+                                                                <p>{prdct.description && prdct.description}</p>
                                                             </div>
                                                             <div className='filtered-item-price'>{prdct.price}</div>
                                                         </div>
-                                                    </li>
+                                                    </Link>
                     )}
                 </ul>}
             { error && input === value && <span className='search-error'>{ error }</span> }
