0ad8f1b5628f8c619705a046bc1bf0fc8b98e46e
inversia <mrs.shpritz@gmail.com>
2020-11-06 03:52:24 +0300

    image resolution gain

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   gifsicle: { optimizationLevel: 2, interlaced: false, colors: 10 },
-  mozjpeg: { progressive: true, quality: 10 },
+  mozjpeg: { progressive: false, quality: 75 },
   pngquant: { quality: [0.25, 0.5] },
   webp: { quality: 10 }
 }
