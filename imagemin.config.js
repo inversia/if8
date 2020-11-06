57aece706981a58a2caef4023fb8a93f570ae9b0
inversia <mrs.shpritz@gmail.com>
2020-11-06 06:14:55 +0300

    wip

--- imagemin.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  gifsicle: { optimizationLevel: 2, interlaced: false, colors: 10 },
-  mozjpeg: { progressive: false, quality: 75 },
-  pngquant: { quality: [0.25, 0.5] },
-  webp: { quality: 10 }
-}
